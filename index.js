1af0c2faaa970612f786ba72fffb5869e01d5ee6
AndroidsDungeon <AndroidsDungeon@droids-iMac.local>
2021-05-07 21:00:12 -0400

    Last Commit

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-console.log('Hello');
+let addToDoButton = document.getElementById('addToDo');
+let toDoContainer = document.getElementById('toDoContainer');
+let inputField = document.getElementById('inputField');
+
+addToDoButton.addEventListener('click', function() {
+  var paragraph = document.createElement('p');
+  paragraph.classList.add('paragraph-styling');
+  paragraph.innerText = inputField.value;
+  toDoContainer.appendChild(paragraph);
+  inputField.value = "";
+  paragraph.addEventListener('click', function() {
+    paragraph.style.textDecoration = "line-through";
+    paragraph.style.color = "#c8dee8";
+  });
+  paragraph.addEventListener('dblclick', function(){
+    toDoContainer.removeChild(paragraph);
+});
+});
+
+function removeAll(){
+    document.getElementById("toDoContainer").innerHTML = "";
+}
+
+document.querySelector("#inputField").addEventListener("keyup", event => {
+  if(event.key !== "Enter") return;
+  document.querySelector("#addToDo").click();
+  event.preventDefault();
+});
